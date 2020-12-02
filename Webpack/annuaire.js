6a7938dbbf5ce1b971aafb588dc54b2eef9d4a69
Camille Verriest <camille.verriest@gmail.com>
2020-12-02 13:57:50 +0100

    added a folder with our new webpack project

---
+++ Webpack/annuaire.js
@@ -0,0 +1,8 @@
+import './static/style.scss';
+
+const radioBtn = document.querySelectorAll('.radio-btn');
+
+radioBtn.addEventListener('click', () => {
+  radioBtn.classList.toggle('radio-filled');
+  alert('toto');
+});
---
+++ Webpack/webpack.config.js
@@ -0,0 +1,103 @@
+// node
+const path = require('path');
+
+// npm
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+// globals
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  mode: 'development',
+  entry: ['./main.js', './annuaire.js'],
+  devtool: 'source-map',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+    new CopyPlugin({
+      patterns: [{
+        from: './static/assets',
+        to: './assets',
+      }, {
+        from: './static/php',
+        to: './',
+      }, {
+        from: './static/html',
+        to: './',
+      }],
+    }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '',
+            },
+
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        // Now we apply rule for images
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+          context: 'public',
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+          context: 'public',
+        },
+      },
+      {
+        test: /.(js|jsx)$/,
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                modules: false,
+              },
+            ],
+          ],
+        },
+      },
+    ],
+  },
+  devServer: {
+    open: true,
+    disableHostCheck: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization',
+    },
+  },
+};

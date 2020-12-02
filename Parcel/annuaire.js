e47964bcada8883c596646072d22d9fefe848b04
Camille Verriest <camille.verriest@gmail.com>
2020-12-02 13:56:04 +0100

    added a folder with parcel and the old project in it

---
+++ Parcel/annuaire.js
@@ -0,0 +1,9 @@
+import './style.scss';
+
+const radioBtn = document.querySelectorAll('.radio-btn');
+
+radioBtn.addEventListener('click', () => {
+  radioBtn.classList.toggle('radio-filled');
+  alert('toto');
+});
+alert('titi');

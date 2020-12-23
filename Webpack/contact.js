081734bfb84c4d8cc1bfb21f22b87836f66ddd84
Camille Verriest <camille.verriest@gmail.com>
2020-12-23 11:59:09 +0100

    delete airtable

--- Webpack/contact.js
+++ Webpack/contact.js
@@ -11,6 +11,16 @@ console.log(headerHTML);
 headerHTML.innerHTML = header;
 footerHTML.innerHTML = footer;
 
+// ***** NAV BAR ANIMATION ON CLICK RESPONSIVE ***** //
+
+const burgerMenu = document.querySelector('.burger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+burgerMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+  burgerMenu.classList.toggle('toggle');
+});
+
 // CONTACT FORM
 
 

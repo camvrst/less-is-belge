eb5816a2b50fb0a67e1774de6b99a1f3b6d5e829
Camille Verriest <camille.verriest@gmail.com>
2021-02-15 10:48:51 +0100

    project cleaned, removed parcel

--- annuaire.js
+++
@@ -1,8 +0,0 @@
-import './style.scss';
-
-const radioBtn = document.querySelectorAll('.radio-btn');
-
-radioBtn.addEventListener('click', () => {
-  radioBtn.classList.toggle('radio-filled');
-  alert('toto');
-});
